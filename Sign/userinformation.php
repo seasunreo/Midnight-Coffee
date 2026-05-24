f07fce5c9379d8cda307c6eb1297167781768864
seasunreo <jeromebago17@gmail.com>
2026-05-24 09:56:38 +0800

    Uploading

---
+++ Sign/userinformation.php
@@ -0,0 +1,17 @@
+<?php
+
+$connection = mysqli_connect('localhost:3000','root');
+
+mysqli_select_db($connection,"youtubedata");
+
+$user = $_POST['user'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+$query = "INSERT INTO `userinfodata`(`user`,`email`,`message`) VALUES ('$user','$email','$message') ";
+
+mysqli_query($connection,$query);
+
+echo "MESSAGE IS SENT";
+
+?>
